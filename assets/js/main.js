ee2cd35fe048ae4fcde5fbccde491a789e53fe50
dalian-expo <1060200619@qq.com>
2026-08-27 09:54:57 +0800

    修复 logo：完全使用用户提供的原图（红D+iEF+UFI 组合图），并恢复完整文件树（之前 read-tree --empty 误删了图片/JSON）

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,7 @@
   function logoBlock() {
     return `
       <a class="brand" href="index.html">
-        <span class="brand-mark">
-          <span class="brand-d">D</span>
-          <span class="brand-ief">IEF</span>
-          <svg class="brand-leaf" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M17 8C8 10 5.9 16.17 3.82 21.34l1.89.66.95-2.3c.48.17.98.3 1.34.3C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8z" fill="#7cb342"/>
-          </svg>
-        </span>
+        <img class="brand-img" src="assets/img/logo-composite.png" alt="大连国际工业博览会 · 中国大连 iEF · UFI 认证" />
         <span class="brand-txt">
           <b>${esc((SITE.name || '').replace(/^2027（.*?）/, '').replace(/[()（）]/g, '').trim() || '大连国际工业博览会')}</b>
           <span>${esc((SITE.enName || 'DALIAN INTERNATIONAL INDUSTRY FAIR').toUpperCase())}</span>
